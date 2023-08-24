a670cc9ac47ac0885ba5de1c5a8499a092e931c3
GoRiJ <jolem824@gmail.com>
2023-08-23 19:06:57 -0600

    first-deploy-4

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,7 +10,7 @@ const error_middleware_1 = require("./middlewares/error.middleware");
 const index_1 = require("./routes/index");
 const config_1 = require("./config/config");
 exports.app = (0, express_1.default)();
-exports.app.use((0, cors_1.default)({ credentials: true, origin: config_1.ACCEPTED_ORIGIN }));
+exports.app.use((0, cors_1.default)({ credentials: true, origin: config_1.ACCEPTED_ORIGIN, methods: ['POST'] }));
 exports.app.use(express_1.default.json());
 exports.app.use(express_1.default.urlencoded({ extended: true }));
 exports.app.use('/api/v1', index_1.router);
