6a99fe610d47f3d73a7587c0cd1439dd2bdfb5f2
GoRiJ <jolem824@gmail.com>
2023-09-05 12:35:35 -0600

    Update config.js

--- dist/js/config/config.js
+++ dist/js/config/config.js
@@ -29,12 +29,10 @@ exports.dbConfig = Object.freeze({
     database: ENV.DB_NAME,
     logging: false,
     synchronize: true,
-    ...(mode === modes.prod && {
-      ssl: {
+    ssl: {
         ca: ENV.SSL_CERT,
         rejectUnauthorized: false
-      }
-    }),
+    },
     entities: [
         entities_1.User,
         entities_1.Patient,
